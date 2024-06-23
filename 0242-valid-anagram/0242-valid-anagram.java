f1fa96bec6000991a2393f6f94eef8b3c7e52f17
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2024-06-23 13:03:35 +0530

    Time: 5 ms (53.79%), Space: 43 MB (76.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) 
+            return false;
         final int ASCII_VALUE_OF_A = 97;
         int[] letters = new int[26];
         for (int i = 0; i < s.length(); i++) {
             letters[s.charAt(i) - ASCII_VALUE_OF_A]++;
-        }
-        for (int i = 0; i < t.length(); i++) {
             letters[t.charAt(i) - ASCII_VALUE_OF_A]--;
         }
         for (int i : letters) {
