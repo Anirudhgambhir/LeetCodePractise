c63290f6ef9b50483eeed0cadde5d0d4e4d3773f
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2024-06-30 00:35:51 +0530

    Time: 1 ms (99.1%), Space: 59.2 MB (40.66%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,24 +1,33 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        return isIncreasingMonotonic(nums, nums.length) 
-            || isDecreasingMonotonic(nums, nums.length);
+        return isDecreasingMonotonic2(nums, nums.length, true) 
+            || isDecreasingMonotonic2(nums, nums.length, false);
     }
 
-    private boolean isIncreasingMonotonic(int[] nums, int length) {
+    // private boolean isIncreasingMonotonic(int[] nums, int length) {
+    //     for (int i = 1; i < length; i++) {
+    //         if (nums[i] > nums[i - 1]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    private boolean isDecreasingMonotonic2(int[] nums, int length, boolean isIncreaseCheck) {
         for (int i = 1; i < length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                return false;
+            if (isIncreaseCheck) {
+                if (nums[i] < nums[i - 1]) {
+                    return false;
+                }
+            }
+            else {
+                if (nums[i] > nums[i - 1]) {
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    private boolean isDecreasingMonotonic(int[] nums, int length) {
-        for (int i = 1; i < length; i++) {
-            if (nums[i] < nums[i - 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    
 }
