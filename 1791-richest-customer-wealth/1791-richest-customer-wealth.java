791913601ad6b2ce87a0faf71cf8d458cef5a34b
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2024-07-20 22:47:06 +0530

    Time: 1 ms (12.16%), Space: 43.2 MB (5.03%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.java
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -1,4 +1,16 @@
 class Solution {
+    // public int maximumWealth(int[][] accounts) {
+    //     int wealthOfRichestCustomer = 0;
+    //     for (int[] account : accounts) {
+    //         int wealthOfCustomer = 0;
+    //         for (int moneyInAccount : account) {
+    //             wealthOfCustomer += moneyInAccount;
+    //         }
+    //         wealthOfRichestCustomer = Math.max(wealthOfCustomer, wealthOfRichestCustomer);
+    //     }
+    //     return wealthOfRichestCustomer;
+    // }
+
     public int maximumWealth(int[][] accounts) {
         int wealthOfRichestCustomer = 0;
         for (int[] account : accounts) {
