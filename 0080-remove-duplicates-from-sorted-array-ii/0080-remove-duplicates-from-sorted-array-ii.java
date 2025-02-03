bc83f96251e724c681d872e4ba118789afd8763a
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2025-02-04 00:25:25 +0530

    Time: 0 ms (100%), Space: 46.9 MB (9.52%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,7 +1,10 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 1, count = 1;
+        // Iterate over the array
         for (int j = 1; j < nums.length; j++) {
+
+            // Use If-Else to find the similar numbers
             if (nums[j] == nums[j-1]) {
                 count++;
             }
@@ -9,6 +12,7 @@ class Solution {
                 count = 1;
             }
 
+            // If they are Less than 2 similar ones we can move ahead.
             if (count <= 2) {
                 nums[i++] = nums[j];
             }
