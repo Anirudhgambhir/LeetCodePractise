1d4e9fcb6389f6b6c0252299cc167ae91b0a7dfd
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2026-01-31 13:04:49 +0530

    Time: 2 ms (89.98%), Space: 47.8 MB (69.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int initialPtr = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[initialPtr++] = nums[i];
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
+                nums[i++] = nums[j];
             }
+            j++;
         }
-        if (initialPtr != 0) {
-            for (int i = initialPtr; i < nums.length; i++) {
-                nums[i] = 0;
-            }
+
+        while (i < nums.length) {
+            nums[i++] = 0;
         }
     }
 }
